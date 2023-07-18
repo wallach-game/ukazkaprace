4fd617c2c6a02bbd3fc0539aa2a3ebf483ae1f82
Jiri Korenek <jirklym@gmail.com>
2023-07-18 02:23:17 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <Modal></Modal>
+    <Items>
+    </Items>
+
+    <Kosik></Kosik>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
+    <Form></Form>
+  </div>
+</template>
+
+<script>
+import Items from "./components/Items.vue";
+import Kosik from "./components/Kosik.vue";
+import Form from "./components/Form.vue";
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Items,
+    Kosik,
+    Form,
+    Modal
+  }
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+
+#app>* {
+  margin-top: 1em;
+}
+</style>
